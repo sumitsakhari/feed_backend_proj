dbf8b2a34a78f4adf792183f5e9fc44b91c64c75
sumitsakhari <sumitsakhari2004@gmail.com>
2026-08-10 23:09:07 +0530

    created create-post page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {CreatePost} from './pages/CreatePost';
+import CreatePost from './pages/CreatePost';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/create-post' element={CreatePost} />
+        <Route path='/create-post' element={<CreatePost />} />
         <Route path='/about' element={<h1>About Page</h1>} />
       </Routes>
     </Router>
