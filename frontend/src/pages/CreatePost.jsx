b4c4538a3ddf130351127bec1db8840175c476fd
sumitsakhari <sumitsakhari2004@gmail.com>
2026-08-11 00:54:34 +0530

    configured tailwind

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const CreatePost = () => {
   return (
-    <section className='create-post-section'>
-        <h1>Create Post</h1>
+    <div className=''>
+        <section class='create-post-section' className=''>
+          <h1 className='mt-2'>Create Post</h1>
 
-        <form>
-            <input type="file" name='image' accept='image/*'/>
-            <input type="text" name='caption' placeholder='give caption' required/>
-            <button type="submit">Submit</button>
-        </form>
-    </section>
+          <form className='bg-gray-600 flex flex-col mt-2'>
+              <input type="file" name='image' accept='image/*'/>
+              <input type="text" name='caption' placeholder='give caption' required/>
+              <button type="submit">Submit</button>
+          </form>
+      </section>
+    </div>
   );
 };
 
